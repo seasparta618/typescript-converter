572a5b41c1f939aa508c61c073d4b71dc87f40b9
Leon Wang <seasparta618@gmail.com>
2024-02-29 22:09:37 +1100

    Define basic AST tree
    After reading the documents, decide to use createSourceFile to define the basic AST, defined a temp.ts to log the error and debug message
    the ecma target version is set to latest

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import * as ts from "typescript";
 
 const convertToObject = (type: string): any => {
-
+    const sourceFile = ts.createSourceFile(
+        "temp.ts",
+        type,
+        ts.ScriptTarget.Latest,
+        false,
+        ts.ScriptKind.TS
+    );
 }
 
 const typeStr = `type Button = {
